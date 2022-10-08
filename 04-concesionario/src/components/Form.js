8864a99c2d6b6cc9ec05a9bb927a861cb2c10baf
JavGuerra <javguerra@badared.com>
2022-10-08 13:09:01 +0200

    concesionario

--- 04-concesionario/src/components/Form.js
+++ 04-concesionario/src/components/Form.js
@@ -1,14 +1,12 @@
 import { useState, useEffect } from "react";
 import React from "react";
-import axios from 'axios';
-import fetchAPI from "../modules/fechapi";
+import { useForm } from 'react-hook-form';
+// import axios from 'axios';
+// import fetchAPI from "../modules/fechapi";
 
 const Form = ({setFilter, setStatus}) => {
 
-    // TODO Todo por hacer aquí
-
-    setFilter('Hola');
-    setStatus(-1);
+    const { register, handleSubmit, formState: { errors } } = useForm();
 
     const [myManufacter, setManufacter] = useState("Marca");
 
@@ -16,19 +14,54 @@ const Form = ({setFilter, setStatus}) => {
         setManufacter(event.target.value)
     }
 
+    const onSubmit = data => {
+        console.log('Datos: ', data);
+        // TODO Preparar y enviar datos del formulario al servidor.
+        // TODO Actualizar status y datos
+
+        setStatus(-1);
+        setFilter('Hola');
+        
+    };
+
+    const chrs = /^[a-zA-ZÀ-ÿ\u00f1\u00d1\s-]*\S$/;
+
+    // TODO sin espacios al inicio y final
+    // TODO si campos vacíos, desactivar botones
+
     return (
-        <form name="formulario" action="#" method="get">
+        <form name="formulario" onSubmit={handleSubmit(onSubmit)}>
         <div className="bg">
-            <label htmlFor="modelo" className="sr">Modelo:</label>
-            <input type="text" id="modelo" name="modelo" placeholder="modelo" autoFocus="autofocus" />
+
+            <label htmlFor="model" className="sr">Modelo:</label>
+            <input type="text" id="model" placeholder="modelo" autoFocus="autofocus"
+                {...register('model', {
+                    pattern: chrs
+                })} />
+
             <label htmlFor="color" className="sr">Color:</label>
-            <input type="text" id="color" name="color" placeholder="color" />
-            <label htmlFor="precio" className="sr">Precio:</label>
-            <input type="number" id="precio" name="precio" placeholder="precio" min="0" />
-            <label htmlFor="marca" className="sr">Marca:</label>
-            <select value={myManufacter} onChange={handleChange} id="marca" name="marca">
+            <input type="text" id="color" placeholder="color"
+                {...register('color', {
+                    pattern: chrs
+                })} />
+
+            <label htmlFor="price" className="sr">Precio:</label>
+            <input type="number" id="price" placeholder="precio" min="0"
+                {...register('price', {
+                    min: 1
+                })} />
+
+            <label htmlFor="brand" className="sr">Marca:</label>
+            <select value={myManufacter} onChange={handleChange} id="brand"
+                {...register('brand')}>
                 <option value="Marca">--Marca--</option>
+                <option value="Dos">Dos</option>
             </select>
+
+            {errors.model?.type === 'pattern' && <p>Modelo incorrecto.</p>}
+            {errors.color?.type === 'pattern' && <p>Color incorrecto.</p>}
+            {errors.price && <p>Precio incorrecto.</p>}
+
         </div>
         <button id="borrar" type="reset">Borrar</button>
         <button id="enviar" type="send">Enviar</button>
