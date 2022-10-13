dd042b7cab47500e84c590ecdb302cfe61b70138
JavGuerra <javguerra@badared.com>
2022-10-13 17:35:03 +0200

    concesionario

--- 04-concesionario/src/components/Status.js
+++ 04-concesionario/src/components/Status.js
@@ -1,12 +1,11 @@
 const Status = ({dataStatus}) => {
 
-    dataStatus = Math.round(dataStatus);
-
     const status = (
         (dataStatus === 0) ? 'OK' :
         (dataStatus === 1) ? 'Sin coincidencias' :
         (dataStatus === 2) ? 'Sin parámetros' :
-        'Error' );
+        'Error'
+    );
 
     return (
         <p>Estatus: {status}</p>
